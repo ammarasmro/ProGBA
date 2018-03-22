a060b65ff8ce01088ec73eb530076ba8afa0e914
ammarasmro <ammarasmaro@gmail.com>
2018-03-21 22:01:50 -0600

    Initial commit

---
+++ src/main/java/ProGBA/ProGbaApplication.java
@@ -0,0 +1,12 @@
+package ProGBA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProGbaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProGbaApplication.class, args);
+	}
+}
