829de085e03ae98f565f8300ba1c41f1ed4e084d
ammarasmro <ammarasmaro@gmail.com>
2018-05-05 13:40:39 -0600

    db: add mongoDB support to gradle
    Signed-off-by: ammarasmro <ammarasmaro@gmail.com>

---
+++ src/main/java/clients/MongoDBClient.java
@@ -0,0 +1,13 @@
+package clients;
+
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoClient;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.ConnectionString;
+import com.mongodb.ServerAddress;
+import com.mongodb.MongoCredential;
+import com.mongodb.MongoClientOptions;
+
+public class MongoDBClient {
+
+}
