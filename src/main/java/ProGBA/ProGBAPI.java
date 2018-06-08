0eda26a618a347b9bcd784d17177e41d00da921c
Ammar Asmro <ammarasmaro@gmail.com>
2018-06-08 00:28:50 -0600

    fix: fixed slf4j nop issue
    Signed-off-by: ammarasmro <ammarasmaro@gmail.com>

--- src/main/java/ProGBA/ProGBAPI.java
+++ src/main/java/ProGBA/ProGBAPI.java
@@ -1,4 +1,11 @@
 package ProGBA;
 
+import static spark.Spark.*;
+
 public class ProGBAPI {
+
+    public static void main(String[] args) {
+        get("/hello", (req, res) -> "Hello World");
+    }
+
 }
